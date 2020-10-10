7195b77350b7ec3c8d79f6b575d973b593500faf
assemberist <schur.alexandr2015@yandex.ru>
2020-10-10 17:28:40 +0300

    clear debugs

--- alchimist-beta-0.1.0/src/library.c
+++ alchimist-beta-0.1.0/src/library.c
@@ -4,7 +4,6 @@ element* dualisation(library* lib, element* element1, element* element2){
 	int i;
 	for(i = lib->recept_count; i--;){
 		combinate* com = lib->recepts + i;
-		printf("ch1: %d ch2: %d el1: %d el2: %d\n", com->reagent1, com->reagent2, element1, element2);
 		if((com->reagent1 == element1 && com->reagent2 == element2) || (com->reagent1 == element2 && com->reagent2 == element1)){
 			com->rezult->is_open = 1;
 			return com->rezult;
