340208bda88f8a66d4f42ac4dee3674ffaa30070
Assemberist <schur.alexandr2015@yandex.ru>
2025-08-07 22:37:47 +0300

    [Archivator] Libaray update
    1) Simlify library
    2) Remove not needed calls
    * get_el_name() - lib simplified.
    * dualisation() - game will use archive,
      so is oudated.
    * search_libs() - not crossplatform +
      concept of game changed
      (multyplayer removed).

--- src/archivator/library.c
+++ src/archivator/library.c
@@ -2,19 +2,6 @@
 #include <stddef.h>
 #include <string.h>
 
-element* dualisation(library* lib, element* element1, element* element2, size_t session){
-	if(element1 && element2){
-		int i;
-		for(i = lib->recept_count; i--;){
-			combinate* com = lib->recepts + i;
-			if((com->reagent1 == element1 && com->reagent2 == element2) || (com->reagent1 == element2 && com->reagent2 == element1)){
-				return com->rezult;
-			}
-		}
-	}
-	return 0;
-}
-
 void dispose_library(library* lib){
 	int i = lib->group_count;
 	while(i--){
@@ -31,32 +18,11 @@ void dispose_library(library* lib){
 	remove_tree(lib->worterbuch);
 }
 
-char* get_el_name(element* ptr){
-	return (ptr->shortName.is_long ? ptr->longName.name : ptr->shortName.name);
-}
-
 void parse_lib(char* src, void* el, void* _arg){
 	size_t len = strlen(src);
 	*((char**)el) = malloc(len);
 	strcpy(*(char**)el, src);
 	(*((char**)el))[len-1] = '\0';
-}
-
-char** search_libs(const char* lib_foulder, size_t* len){
-	size_t path_len = sizeof(lib_foulder);
-
-	char buffer[512];
-	strcpy(buffer, "find ");
-	strcat(buffer, lib_foulder);
-	strcat(buffer, "* -maxdepth 0 -type d > ");
-	strcat(buffer, lib_foulder);
-	strcat(buffer, "library_list.txt");
-	system(buffer);
-
-	strcpy(buffer, lib_foulder);
-	strcat(buffer, "library_list.txt");
-
-	return read_file(buffer, parse_lib, sizeof(char*), NULL, len);
 }
 
 void* read_file(char* path, void(*parser)(char*, void*, void*), size_t element_size, void* args, size_t* rows){
--- src/archivator/library.h
+++ src/archivator/library.h
@@ -7,30 +7,22 @@
 #include <stdint.h>
 #include "../submod/string_tree/src/string_tree.h"
 
-typedef union element{
-	struct{
-		char* name;
-	} longName;
-	struct{
-		char name[sizeof(char*) * 2 - 1];
-		uint8_t is_long:1;
-		uint8_t is_open:7;
-	} shortName;
-}element;
-
-char* get_el_name(element* ptr);
+typedef struct element{
+	char* name;
+	uint8_t is_open:1;
+} element;
 
 typedef struct group{
 	char* name;
 	element* names;
 	size_t name_count;
-}group;
+} group;
 
 typedef struct combinate{
 	element* reagent1;
 	element* reagent2;
 	element* rezult;
-}combinate;
+} combinate;
 
 typedef struct library{
 	combinate* recepts;
@@ -38,11 +30,9 @@ typedef struct library{
 	group* groups;
 	size_t group_count;
 	token* worterbuch;
-}library;
+} library;
 
 void* read_file(char* path, void(*parser)(char*, void*, void*), size_t element_size, void* args, size_t* rows);
-char** search_libs(const char* lib_foulder, size_t* len);
-element* dualisation(library* lib, element* element1, element* element2, size_t session);
 void dispose_library(library* lib);
 
 #endif
