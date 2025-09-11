f4af9f280ca5f457d34af9c6a3bf695d4d7a9b99
assemberist <schur.alexandr2015@yandex.ru>
2025-09-11 21:11:38 +0200

    [builder] Script builder added

---
+++ core/main.c
@@ -0,0 +1,7 @@
+#include "api.h"
+
+int main(int argc, char** argv){ 
+    if(argc >= 2)
+        new_game(argv[1]);
+    
+}
--- core/test.c
+++
@@ -1,3 +0,0 @@
-#include "api.h"
-
-int main(int argc, char** argv){ return argc >= 2 ? new_game(argv[1]) : 0; }
