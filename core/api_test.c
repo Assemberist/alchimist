03788b2fc13671d97f5a4470f2fc8200266d3b6c
assemberist <schur.alexandr2015@yandex.ru>
2025-10-07 11:18:20 +0200

    [Test] New test library

---
+++ core/api_test.c
@@ -0,0 +1,78 @@
+#include "api.h"
+#include <stdio.h>
+
+/*
+char* partial_match_groups(const char* group);
+char* partial_match_elements(const char* element);
+char* check_combination(const char* elem1, const char* elem2);
+combination find_combinations_for_element(const char* element);
+*/
+
+int main(int argc, char** argv){ 
+    if(argc == 1) return -1;
+
+    if(new_game(argv[1])){
+        {
+            puts("Element list:");
+            for(char* ptr = list_elements(); ptr; ptr = get_rest())
+                puts(ptr);
+
+            puts("\nGroup list:");
+            size_t i = 0;
+            for(char* ptr = list_groups(); ptr; ptr = get_rest()){
+                puts(ptr);
+                i++;
+            }
+
+            char* groups[i];
+            i = 0;
+            for(char* ptr = list_groups(); ptr; ptr = get_rest())
+                groups[i++] = ptr;
+
+            puts("\nLook groups:");
+            for(int j = 0; j < i; j++){
+                puts(groups[j]);
+                for(char* ptr = look_group(groups[j]); ptr; ptr = get_rest())
+                    puts(ptr);
+            }
+
+            puts("\nList combinations");
+            for(combination combo = list_combinations(); combo.reagent1; combo = next_combination())
+                printf("%s + %s = %s\n", combo.reagent1, combo.reagent2, combo.result);
+        }
+
+        puts("\nOpen all\n");
+        openAll();
+
+        {
+            puts("\nElement list:");
+            for(char* ptr = list_elements(); ptr; ptr = get_rest())
+                puts(ptr);
+
+            puts("\nGroup list:");
+            size_t i = 0;
+            for(char* ptr = list_groups(); ptr; ptr = get_rest()){
+                puts(ptr);
+                i++;
+            }
+
+            char* groups[i];
+            i = 0;
+            for(char* ptr = list_groups(); ptr; ptr = get_rest())
+                groups[i++] = ptr;
+
+            puts("\nLook groups:");
+            for(int j = 0; j < i; j++){
+                puts(groups[j]);
+                for(char* ptr = look_group(groups[j]); ptr; ptr = get_rest())
+                    puts(ptr);
+                puts("");
+            }
+
+            puts("\nList combinations");
+            for(combination combo = list_combinations(); combo.reagent1; combo = next_combination())
+                printf("%s + %s = %s\n", combo.reagent1, combo.reagent2, combo.result);
+
+        }
+    }
+}
